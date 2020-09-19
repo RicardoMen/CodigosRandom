373a5722e6e531f399c92dc272e19f7e10d80537
RicardoMen <39391488+RicardoMen@users.noreply.github.com>
2020-09-19 18:06:13 -0500

    Add files via upload

---
+++ CreadorDeObstaculos.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreadorDeObstaculos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] Obstaculos;
+    
+    public float PosXIni;
+    public float PosXFinal;
+
+
+    public float PosYIni;
+    public float PosYFinal;
+
+    public float PosZini;
+    public float PosZFinal;
+
+    public float TiempoDeCreacion;
+     float timer;
+
+    
+    void Start()
+    {
+        timer = 0 ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+
+        if (timer >= TiempoDeCreacion) 
+        {
+
+            CreadorDeTropiezos();
+            timer = 0;
+        
+        }
+
+    }
+    public void CreadorDeTropiezos() 
+    {
+        GameObject ObstaculoGenerado;
+        int index;
+
+        float RandomX;
+        float randomY;
+        float randomZ;
+
+
+        index = Random.Range(0, Obstaculos.Length);
+        RandomX = Random.Range(PosXIni, PosXFinal);
+        randomY = Random.Range(PosYIni, PosYFinal);
+        randomZ = Random.Range(PosZini, PosZFinal);
+        ObstaculoGenerado = Obstaculos[index];
+
+
+
+        Instantiate(ObstaculoGenerado, new Vector3(RandomX, randomY, randomZ), Quaternion.identity);
+    
+    }
+}
---
+++ CreadorInstantaneo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreadorInstantaneo: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] Obstaculos;
+    public float CantidadDeobjetos;
+
+  
+
+    GameObject ObstaculoGenerado;
+
+    public float Salto;
+    public float DistanciaEntre;
+
+
+    void Start()
+    {
+        for (int i = 0; i < CantidadDeobjetos; i++)
+        {
+            int index;
+            index = Random.Range(0, Obstaculos.Length);
+            ObstaculoGenerado = Obstaculos[index];
+
+            Instantiate(ObstaculoGenerado, new Vector3(Random.Range(gameObject.transform.position.x - DistanciaEntre, gameObject.transform.position.x + DistanciaEntre), gameObject.transform.position.y, gameObject.transform.position.z),Quaternion.identity);
+            gameObject.transform.position += new Vector3(0, 0, Salto);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      /*  if (CantidadDeobjetos > 0) 
+        {
+            CreadorDeTropiezos();
+            CantidadDeobjetos--;
+        }*/
+    }
+   
+}
